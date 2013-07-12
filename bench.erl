c6505627e9d20b65bd5461e85497c212cfb664fc
Ignas Vyšniauskas <ignas.vysniauskas@spilgames.com>
2013-07-12 15:05:26 +0200

    Disable erlsom in benchmark

--- bench.erl
+++ bench.erl
@@ -17,15 +17,15 @@ main([Path, Count_]) ->
   spawn(fun() -> loop_xmerl(List, Count), 
     Self ! {ready, xmerl, timer:now_diff(erlang:now(),T0),process_info(self(),memory)} end),
 
-  spawn(fun() ->
-    code:add_pathz("erlsom/ebin"),
-    case code:load_file(erlsom) of
-      {error, _} -> Self ! {ready, erlsom, 9999999, {memory,0}};
-      _ ->
-        loop_erlsom(Bin, Count), 
-        Self ! {ready, erlsom, timer:now_diff(erlang:now(),T0),process_info(self(),memory)} 
-    end
-  end),
+%%  spawn(fun() ->
+%%    code:add_pathz("erlsom/ebin"),
+%%    case code:load_file(erlsom) of
+%%      {error, _} -> Self ! {ready, erlsom, 9999999, {memory,0}};
+%%      _ ->
+%%        loop_erlsom(Bin, Count), 
+%%        Self ! {ready, erlsom, timer:now_diff(erlang:now(),T0),process_info(self(),memory)} 
+%%    end
+%%  end),
 
 
   Size = Count*size(Bin),
@@ -35,9 +35,9 @@ main([Path, Count_]) ->
   receive
     {ready,parsexml,T2,{memory,M2}} -> io:format("parsexml: ~8.. Bms ~8.. BKB ~BMB/s~n", [T2 div 1000,M2 div 1024, Size div T2])
   end,
-  receive
-    {ready,  erlsom,T3,{memory,M3}} -> io:format("  erlsom: ~8.. Bms ~8.. BKB ~BMB/s~n", [T3 div 1000,M3 div 1024, Size div T3])
-  end,
+%%  receive
+%%    {ready,  erlsom,T3,{memory,M3}} -> io:format("  erlsom: ~8.. Bms ~8.. BKB ~BMB/s~n", [T3 div 1000,M3 div 1024, Size div T3])
+%%  end,
   ok.
 
 loop_parsexml(_Bin,0) -> ok;
@@ -50,8 +50,8 @@ loop_xmerl(Bin,Count) ->
   _A = xmerl_scan:string(Bin),
   loop_xmerl(Bin,Count-1).
 
-loop_erlsom(_,0) -> ok;
-loop_erlsom(Bin,Count) ->
-  _A = erlsom:simple_form(Bin),
-  loop_erlsom(Bin,Count-1).
+%%loop_erlsom(_,0) -> ok;
+%%loop_erlsom(Bin,Count) ->
+%%  _A = erlsom:simple_form(Bin),
+%%  loop_erlsom(Bin,Count-1).
 
